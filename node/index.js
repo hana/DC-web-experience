737fc5a05eb3cd81700fe2bb8e67684ba14bb326
hana <hana.1995@mac.com>
2020-05-06 00:29:44 -0400

    Server v0.0.2

--- node/index.js
+++ node/index.js
@@ -7,30 +7,53 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+const users = {}; 
 
 io.on('connection', (socket) => {
-    const id = socket.id;
-    const data = {
-        "elm1":1, 
-        "elm2": 0.87,
-        "elm3": "foo"
-    };
-
+    const id = String(socket.id);
 
     socket.on('/user/enter', (msg) => {
         console.log(`${msg} came in.`);
-        console.log(id);
-        io.to(id).emit('/json', data);
-        // socket.emit('/json', data);
+        
+        for(user in users)  {
+            console.log(user);
+            io.to(id).emit('/user/enter', user);
+        }
+        
+        users[id] = 1;
+        socket.broadcast.emit('/user/enter', id);
     })
 
     socket.on('/user/leave', (msg) => {
         console.log(`${msg} has left.`);
+        delete users[id];
+        socket.broadcast.emit('/user/leave', id);
     })
 
     socket.on('/broadcast', (msg) => {
         socket.broadcast.emit("/broadcast", msg);
     })
+
+    socket.on('/user/position', (pos) => {        
+        const data = {
+            id: id,
+            x:pos.x,
+            y:pos.y,
+            z:pos.z
+        }
+        socket.broadcast.emit("/user/position", data);
+    })
+
+    socket.on('/user/rotation', (vec) => {
+        const data = {
+            id: id,
+            x:vec._x,
+            y:vec._y,
+            z:vec._z
+        }
+        socket.broadcast.emit("/user/rotation", data);
+    })
+
 
     socket.on('disconnect', () => {
         console.log("A user disconnect");
